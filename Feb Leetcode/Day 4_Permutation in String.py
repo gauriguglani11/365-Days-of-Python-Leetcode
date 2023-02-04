ca4822499df4f58dada8ba24dfaced3b9486b764
Gauri Guglani <63574359+gauriguglani11@users.noreply.github.com>
2023-02-04 21:00:36 +0530

    Day 4

--- Feb Leetcode/Day 4_Permutation in String.py
+++ Feb Leetcode/Day 4_Permutation in String.py
@@ -1,13 +1,18 @@
-def checkInclusion(self, s1: str, s2: str) -> bool:
-	cntr, w = Counter(s1), len(s1)   
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        cntr, w, matched = Counter(s1), len(s1), 0   
 
-	for i in range(len(s2)):
-		if s2[i] in cntr: 
-			cntr[s2[i]] -= 1
-		if i >= w and s2[i-w] in cntr: 
-			cntr[s2[i-w]] += 1
+        for i in range(len(s2)):
+            if s2[i] in cntr: 
+                cntr[s2[i]] -= 1
+                if cntr[s2[i]] == 0:
+                    matched += 1
+            if i >= w and s2[i-w] in cntr: 
+                if cntr[s2[i-w]] == 0:
+                    matched -= 1
+                cntr[s2[i-w]] += 1
 
-		if all([cntr[i] == 0 for i in cntr]): # see optimized code below
-			return True
+            if matched == len(cntr):
+                return True
 
-	return False
+        return False
